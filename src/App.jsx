12c01f504c88a7e9942e7376fc2e42aab1194848
igor.kudlo <igoreklida@gmail.com>
2024-05-30 17:59:46 +0300

    request data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import Header from "./components/Header/Header.jsx";
 import Main from "./pages/Main/Main.jsx";
+import {getCoins} from "./api/api.js";
 
 function App() {
     const [balance, setBalance] = useState(5000);
+    const [coins, setCoins] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await getCoins();
+            setCoins(data.coins)
+        }
+        fetchData();
+    }, [])
 
     return <>
         <Header />
-        <Main balance={balance} setBalance={setBalance} />
+        <Main coins={coins} balance={balance} setBalance={setBalance} />
     </>
 }
 
